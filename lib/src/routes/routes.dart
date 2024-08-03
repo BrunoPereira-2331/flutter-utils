1d8ff10c664f1cb90451deebec13224e81d447c0
Bruno Pereira <bruno.pereira@brudam.com.br>
2024-08-02 22:35:38 -0300

    created getDefaultRoute function & getHome function in routes class

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,21 +4,29 @@ import 'package:flutter_utils/src/layout/screens/home_screen.dart';
 import 'package:flutter_utils/src/layout/screens/login_screen.dart';
 
 class Routes {
+
   static Route<dynamic>? generate(RouteSettings settings) {
     dynamic screen;
 
     try {
       switch (settings.name) {
         case "/home":
-          screen =  const HomeScreen();
+          screen = const HomeScreen();
           break;
         default:
-          screen =  const LoginScreen();
+          screen = const LoginScreen();
       }
     } catch (e) {
       screen = const ErrorScreen();
     }
     return MaterialPageRoute(builder: (_) => const HomeScreen());
+  }
 
+  static Route<dynamic>? getDefaultRoute() {
+    return MaterialPageRoute(builder: (_) => Container());
+  }
+
+  static Widget getHome() {
+    return const LoginScreen();
   }
 }
